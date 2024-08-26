d2308b81ed79e695b7844290e405429bebb7a482
ozibryan <ozibryan@yahoo.com>
2024-08-26 16:50:36 +1000

    CMD I/F Commit

--- routes/product.route.js
+++ routes/product.route.js
@@ -4,9 +4,13 @@ const router = express.Router();
 const { getProducts, getProduct, createProduct, updateProduct, deleteProduct } = require('../controller/product.controller.js');
 
 
+// Get All products
 router.get('/', getProducts);
+
+// Get specific Product by Id
 router.get("/:id", getProduct);
 
+// Create new product
 router.post("/", createProduct);
 
 // update a product
@@ -14,8 +18,6 @@ router.put("/:id", updateProduct);
 
 // delete a product
 router.delete("/:id", deleteProduct);
-
-
 
 
 module.exports = router;
